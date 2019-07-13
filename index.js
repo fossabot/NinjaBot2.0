ae9a98342053c5a4e337c83ae200c77947137594
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-12 22:30:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ if (message.content.toLowerCase().startsWith(config.prefix + `add`)) {
   message.delete().catch();	 
    let hEmbed = new Discord.RichEmbed()
    .setTitle("Ninja Bot Commands")
-   .setDescription("Below is a list of my commands and their usage.)
+   .setDescription("Below is a list of my commands and their usage.")
    .setColor("0xff80ff")
    .addField("Required", "``{}`` -> Required Filed")
    .addField("Not Required", "``[] -> Not Required Field")
