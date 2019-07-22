1eafbb008bfe159f383ddbb799de28294f962e2f
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-22 01:59:21 -0600

    Update "Memes" Command To Embed

--- index.js
+++ index.js
@@ -142,13 +142,12 @@ let Tembed4 = new Discord.RichEmbed()
         c.send({ embed: embed });
     }).catch(console.error);
 }
-	
-  if(command === "ping") {	  
-      // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+
+if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
-    m.edit(m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  });
+    m.edit(`:ping_pong: Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
 	
 if(command === "invite") {
