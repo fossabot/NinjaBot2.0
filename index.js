ef16ba1895bbaf5c6d9d69820a0316b6593703b9
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-26 02:31:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -549,6 +549,24 @@ if(command === "rr") {
        console.log(`<@${message.author.id}> has used the setprefix command in ${message.channel.guild} channel ${message.channel}`)
     }
 
+if(command === "nr") {
+  message.delete().catch();	
+   let iEmbed = new Discord.RichEmbed()
+   .setTitle("Ninja Support Server Rules")
+   .setColor("#0x3dfbff")
+   .addField("#1 - Respect", "Be respectful to all Admins, Moderators & Members")
+   .addField("#2 - Screen Sharing", "Do not screen share this guild or staff direct messages to you with other guilds ``INSTANT BAN``")
+   .addField("#3 - Racism", "**NO** racism or otherwise offensive remarks ``INSTANT BAN``")
+   .addField("#4 - Personal Info", "**DO NOT** release personal information ``SSN, CC ect`` in this server ``INSTANT BAN``")
+   .addField("#5 - Hacker Talk", "No DDOS, IP Grabber or Booster talk")
+   .addField("#6 - Channel Flooding", "**DO NOT** flood channels with text, emoji or multi line spam ``INSTANT BAN``") 
+   .addField("#7 - NSFW", "This **IS NOT** a NSFW friendly server") 
+   .addField("#8 - DM Spam", "**DO NOT** spam advertise members in their DMs")
+   .addField("#9 -
+   .addField("#10 -
+   message.channel.send(iEmbed)
+ }	
+
   });
 
 
