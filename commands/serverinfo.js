9b7ffb251de18a91685b95a74431c57761ce7b34
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 14:26:53 -0600

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,25 +2,67 @@ const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
 const owners = require ("../data/config.json");
 
-exports.run = async (client, message, args) => {
-	
-let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setAuthor(`${message.guild.name} - Informations`, message.guild.iconURL)
-    .setDescription("Server Information")
-    .setColor("0x0ffff")
-    .setThumbnail(sicon)
-    .addField("Server Owner", message.guild.owner, true)
-    .addField("Server Name", message.guild.name, true)
-    .addField('Server region', message.guild.region, true)
-    .addField("Created On", message.guild.createdAt, true)
-    .addField("You Joined", message.member.joinedAt, true)
-    .addField('Channel count', message.guild.channels.size, true)
-    .addField('Total member count', message.guild.memberCount)
-    .addField('Verification level', message.guild.verificationLevel, true)
-    .setFooter('Guild created at:')
-    .setTimestamp(message.guild.createdAt);
+function checkDays(date) {
+    let now = new Date();
+    let diff = now.getTime() - date.getTime();
+    let days = Math.floor(diff / 86400000);
+    return days + (days == 1 ? " day" : " days") + " ago";
+};
+exports.run = (client, message, args) => {
+    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+    let region = {
+        "brazil": "Brazil",
+        "eu-central": "Central Europe",
+        "singapore": "Singapore",
+        "us-central": "U.S. Central",
+        "sydney": "Sydney",
+        "us-east": "U.S. East",
+        "us-south": "U.S. South",
+        "us-west": "U.S. West",
+        "eu-west": "Western Europe",
+        "vip-us-east": "VIP U.S. East",
+        "london": "London",
+        "amsterdam": "Amsterdam",
+        "hongkong": "Hong Kong"
+    };
+    
+    var emojis;
+    if (message.guild.emojis.size === 0) {
+        emojis = 'None';
+    } else {
+        emojis = message.guild.emojis.size;
+    }
 
-    message.channel.send(serverembed);
-       message.delete().catch();
- };
+    const embed = new Discord.RichEmbed()
+  .setAuthor(message.guild.name, message.guild.iconURL ? message.guild.iconURL : client.user.displayAvatarURL)
+  .setThumbnail(message.guild.iconURL)
+  .setTimestamp()
+  .addField("Created", `${message.guild.createdAt.toString().substr(0, 15)},\n(${checkDays(message.guild.createdAt)})`, true)
+  .addField("ID", message.guild.id, true)
+  .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+  .addField("Region", region[message.guild.region], true)
+  .addField("User Count", message.guild.memberCount, true)
+  .addField("Member Count", message.guild.members.filter(m => !m.user.bot).size, true)
+  .addField("Bot Count", message.guild.members.filter(m => m.user.bot).size, true)
+  .addField("AFK Timeout", message.guild.afkTimeout / 60 + ' minutes', true)
+  .addField("Roles", message.guild.roles.size, true)
+  .addField("Channels", message.guild.channels.size, true)
+  .addField("Emojis", `${emojis}/100`, true)
+  .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
+  .setColor(Math.floor(Math.random()*16777215))
+  .setFooter(`© Ninja Bot v1.00`);
+  message.channel.send({embed});
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["serverstats"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'serverinfo',
+  description: 'Displays information about the server.',
+  usage: 'serverinfo'
+};
