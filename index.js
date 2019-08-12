33f3147fb5f55b46b14771e2a7a8d29686eff46b
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 00:16:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,19 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
+const requireAll = require('require-all');   // Don't forget to install!
+
+const files = requireAll({                   // Require all the files within your
+  dirname: `${__dirname}/events`,            // event directory which have a name
+  filter: /^(?!-)(.+)\.js$/                  // ending in '.js' NOT starting
+});                                          // with '-' (a way to disable files).
+
+client.removeAllListeners();                 // Prevent duplicate listeners on reload.
+
+for (const name in files) {                  // Iterate through the files object
+  const event = files[name];
+}
+
 Music.start(client, {
   youtubeKey: "AIzaSyDu_YZn7ivq66a3baryXztxK8rFrERAKvA",
   prefix: config.prefix, // Prefix for the commands.
