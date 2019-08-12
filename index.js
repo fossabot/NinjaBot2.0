0952aad71f19b241a9db28002f9cf55a99f1acfc
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 12:20:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ client.on('message', async message => {
   const prefixes = ['nb/', `<@!?${client.user.id}> `];
   const prefixRegex = new RegExp(`^(${prefixes.join('|')})`);
   const prefix = message.content.match(prefixRegex);
+
+const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+const command = args.shift();
 })
 
 //Stops the bot from responding to other bots.
