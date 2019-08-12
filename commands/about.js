49db9d301289ff8ecc0a4ddaa40a14c3d8446a75
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 12:30:17 -0600

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -26,5 +26,5 @@ let owner = client.users.get('444609097233465347');
    .addField("Ninja Bot Support", "[Support Server](https://discord.gg/yFmtAUM)")
    message.channel.send(aEmbed).then(sentMessage => {
 	sentMessage.react('👍');
- };
+ });
 }
