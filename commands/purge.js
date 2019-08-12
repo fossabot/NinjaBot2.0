13d8bb980d96e0017a0009168a2868503c7c938b
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 13:29:38 -0600

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -13,18 +13,21 @@ const snekfetch = require('snekfetch');
 exports.run = async (client, message, args) => {
  
 //This is your Command or Discord Rich Embed code Line followed by the end of the command. OR close "}" bracket
-message.delete().catch();	  
-    // This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
+message.delete().catch();
+        let messagecount = parseInt(args[1]) || 1;
+
+        const deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (message.author.id == bot.user.id) {
+                    message.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    }	  
